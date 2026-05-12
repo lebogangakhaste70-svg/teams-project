0e86009599acc0dc41e1d71715303a4ec3908505
lebogangakhaste70-svg <lebogangakhaste70@gmail.com>
2026-05-12 14:35:47 +0200

    Updated analysis and added dataset files

--- analysis.py
+++ analysis.py
@@ -1,13 +1,4 @@
+
 import pandas as pd
-import matplotlib.pyplot as plt
-
-data = pd.DataFrame({
-    "Year":[2020,2021,2022,2023],
-    "Sales":[100,150,200,250]
-})
-
-print(data)
-
-data.plot(x="Year", y="Sales", kind="line")
-
-plt.show()
+teams = pd.read_csv("teams.csv")
+print(teams.head())
